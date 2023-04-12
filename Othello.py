9a11884b5b73cbc9caecdc13d493af6fb69d7e7e
Vadim91200 <kamenets@hotmail.fr>
2023-04-12 22:45:56 +0200

    Graphical interface working for the current stage of the projet i.e no move function

--- Othello.py
+++ Othello.py
@@ -4,22 +4,9 @@ Created on Wed Apr 12 12:50:38 2023
 
 @author: Charles-André
 """
-from board import Board
+from interface import Interface
 def main():
-    OthelloBoard = Board()
-    current_color = 1
-    tour = 1
-    while tour <=60:
-        print(OthelloBoard)
-        move = input(f'{"Black" if current_color == 1 else "White"} to play (x, y): ')
-        x, y = map(int, move.strip().split())
-
-        if OthelloBoard.valid_move(x, y, current_color):
-            print(OthelloBoard.valid_move(x, y, current_color))
-            current_color = -1 if current_color == 1 else 1
-            tour+=1
-        else:
-            print('Invalid move, try again.')
-
+    OthelloBoard = Interface()
+    OthelloBoard.run()
 if __name__ == "__main__":
     main()
